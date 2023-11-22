0c704dde4a597406a69f47a0330530f43adbc777
Theo2801 <theologos.bourloglou@gmail.com>
2023-11-22 19:14:24 +0200

    [feat] add tailwind, alpine, component logic, tailwindui components

---
+++ app.js
@@ -0,0 +1,17 @@
+/**
+ * Fetch and load components into specified element IDs.
+ *
+ * @param {Array} components - An array of component objects with 'name' and 'elementId' properties.
+ * @returns {void}
+ */
+const fetchComponents = (components) => {
+  components.forEach((component) => {
+    const { name, elementId } = component;
+    fetch(`components/${name}.html`)
+      .then((response) => response.text())
+      .then((data) => {
+        const element = document.getElementById(elementId);
+        element ? (element.innerHTML = data) : "";
+      });
+  });
+};
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        vblue: "#1e1452",
+        vgreen: "#6fdb13"
+      },
+    }
+  },
+  plugins: [],
+}
+
